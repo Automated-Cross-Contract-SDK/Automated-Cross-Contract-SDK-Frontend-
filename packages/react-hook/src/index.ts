15173c53a1842d9bb3dbe9d43c7a22e4ecbaa0e0
Michael Musa <micmusjnr20@gmail.com>
2026-07-13 17:51:28 +0000

    feat(react-hook): add barrel exports for public API

---
+++ packages/react-hook/src/index.ts
@@ -0,0 +1,10 @@
+export { SorobanResurrectProvider, useSorobanResurrectContext } from './SorobanResurrectContext.js'
+
+export { useSorobanResurrect } from './useSorobanResurrect.js'
+
+export type { SorobanResurrectProviderProps } from './SorobanResurrectContext.js'
+
+export type {
+  UseSorobanResurrectOptions,
+  UseSorobanResurrectReturn,
+} from './useSorobanResurrect.js'
